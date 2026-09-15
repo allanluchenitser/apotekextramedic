2bf021baf3348b737135317cf897a654cd6cace0
Allan Luchenitser <aluchenitser@gmail.com>
2026-09-15 09:26:28 +0700

    router and rotates

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
-// import PracticeView from '@/views/PracticeView.vue'
+import { shallowRef } from 'vue';
+
+import PracticeView from '@/views/PracticeView.vue'
 import ApotekView from '@/views/ApotekView.vue'
+import ThreeView from '@/views/ThreeView.vue'
+
+const views = {
+  ApotekView,
+  PracticeView,
+  ThreeView
+}
+
 </script>
 
 <template>
   <main class="app">
-    <ApotekView />
+    <nav class="
+      view-selection
+      absolute bottom-1 right-1
+      bg-white
+      flex
+      text-xs
+      p-2
+      [&>a]:hover:underline [&>a]:hover:cursor-pointer [&>a]:px-2
+      z-1
+    ">
+      <router-link to="/apotek">apotek</router-link>
+      <router-link to="/three">three</router-link>
+      <router-link to="/practice">practice</router-link>
+    </nav>
+    <router-view></router-view>
   </main>
 </template>
 
@@ -13,5 +37,13 @@ import ApotekView from '@/views/ApotekView.vue'
 :root {
   --left-sidebar-width: 325px;
   --right-sidebar-width: 325px;
+}
+
+.app {
+  main {
+    width: 100%;
+    height: 100%;
+    min-height: 100vh;
+  }
 }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,24 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
 import './style.css'
 import App from './App.vue'
 
+import ApotekView from './views/ApotekView.vue';
+import PracticeView from './views/PracticeView.vue';
+import ThreeView from './views/ThreeView.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/apotek'},
+    { path: '/apotek', component: ApotekView },
+    { path: '/practice', component: PracticeView },
+    { path: '/three', component: ThreeView },
+  ]
+})
+
 const app = createApp(App)
+app.use(router);
 
 app.mount('#app');
