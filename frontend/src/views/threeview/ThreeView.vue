89227ba010b721e51e4915ea2fe5b481f949a11f
Allan Luchenitser <aluchenitser@gmail.com>
2026-09-16 20:53:11 +0700

    experimenting with three textures and materials

--- frontend/src/views/threeview/ThreeView.vue
+++ frontend/src/views/threeview/ThreeView.vue
@@ -2,16 +2,17 @@
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 
 import * as THREE from 'three';
-import type { RotationDegrees } from '@/js/localTypes';
 
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import apotekGlbUrl from '@/assets/glb/apotek_tres.glb?url'
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 import DisplayCoordinates from '@/components/DisplayCoordinates.vue';
 
-const displayPosition = ref<{ position: THREE.Vector3, rotation: RotationDegrees }>({
+import type { RotationDegrees } from '@/js/localTypes';
+
+const cameraInfo = ref<{ position: THREE.Vector3, rotation: RotationDegrees }>({
   position: new THREE.Vector3(0, 0, 0),
   rotation: { x: 0, y: 0, z: 0 }
 })
@@ -23,18 +24,21 @@ let scene: THREE.Scene
 let camera: THREE.PerspectiveCamera
 let controls: OrbitControls
 
+const startingCameraPosition: [number, number, number] = [2, 2, 5] as const;
+const startingCameraTarget: [number, number, number] = [0, 0, 0] as const;
+
 onMounted(() => {
   if (!threeView.value) return
 
   // ------ SETUP
-
   const width = threeView.value.clientWidth
   const height = threeView.value.clientHeight
 
   scene = new THREE.Scene()
   camera = new THREE.PerspectiveCamera(30, width / height, 0.1, 1000)
 
-  camera.position.set(2, 2, 10);
+  // target is set by OrbitControls, not here
+  camera.position.set(...startingCameraPosition);
 
   renderer = new THREE.WebGLRenderer({
     // antialias: true,
@@ -46,31 +50,107 @@ onMounted(() => {
 
   threeView.value.appendChild(renderer.domElement);
 
-  // ------ OBJECTS
+  // ------ MATERIAL
 
-  // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  const testColor = 0x3a1af1;
 
-  // const material = new THREE.MeshBasicMaterial({
-  //   color: 0x3a1af1
-  // });
-
-  // const material = new THREE.MeshStandardMaterial({
-  //   color: 0x3a1af1
-  // });
-
-  // const cube = new THREE.Mesh( geometry, material );
-
-  const loader = new GLTFLoader();
-  loader.load(apotekGlbUrl, (gltf) => {
-    scene.add(gltf.scene);
+  const basicMaterial = new THREE.MeshBasicMaterial({
+    color: testColor
   });
 
-  // scene.add( cube );
+  const lambertMaterial = new THREE.MeshLambertMaterial({
+    color: testColor
+  });
+
+  const phongMaterial = new THREE.MeshPhongMaterial({
+    color: testColor,
+    specular: 0xff0000, // color not quantity
+  });
+
+  const standardMaterial = new THREE.MeshStandardMaterial({
+    color: testColor,
+    // roughness: 0.5,
+    metalness: .5,
+  });
+
+  const physicalMaterial = new THREE.MeshPhysicalMaterial({
+    color: testColor
+  });
+
+  // const material
+  //   // = basicMaterial;
+  //   = lambertMaterial;
+  //   // = phongMaterial;
+  //   // = standardMaterial;
+  //   // = physicalMaterial;
+
+  // ------ GEOMETRY
+
+  const geometry = new THREE.SphereGeometry( 0.5, 64, 64 );
+
+  // ------ MESH
+
+  const mesh = new THREE.Mesh( geometry, lambertMaterial );
+  const mesh2 = new THREE.Mesh( geometry, phongMaterial );
+  const mesh3 = new THREE.Mesh( geometry, standardMaterial );
+  const mesh4 = new THREE.Mesh( geometry, physicalMaterial );
+
+  mesh.position.set(-2.25, 0, 0);
+  mesh2.position.set(-0.75, 0, 0);
+  mesh3.position.set(0.75, 0, 0);
+  mesh4.position.set(2.25, 0, 0);
+
+  // ------ LIGHT SOURCE
+
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 20);
+  directionalLight.position.set(1, 3, 2.25);
+
+  // const pointLight = new THREE.PointLight(0xffffff, 1);
+  // pointLight.position.set(5, 5, 5);
+
+  // const spotLight = new THREE.SpotLight(0xffffff, 1);
+  // spotLight.position.set(5, 10, 5);
+
+  // const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444, 1);
+  // hemisphereLight.position.set(0, 20, 0);
+
+  const light
+    // = ambientLight;
+    = directionalLight;
+    // = pointLight;
+    // = hemisphereLight;
+    // = spotLight;
+
+  const lightHelper
+    = new THREE.DirectionalLightHelper(light);
+    // = new THREE.PointLightHelper(light);
+    // = new THREE.SpotLightHelper(light);
+    // = new THREE.HemisphereLightHelper(light);
+
+  scene.add(
+    mesh,
+    mesh2,
+    mesh3,
+    mesh4,
+    light,
+    ambientLight,
+  );
+
+  if (lightHelper) scene.add(lightHelper);
+
+  // ------ EXTERNAL MESH
+
+  // const loader = new GLTFLoader();
+  // loader.load(apotekGlbUrl, (gltf) => {
+  //   scene.add(gltf.scene);
+  // });
 
   // ------ ANIMATION, CONTROLS
 
   controls = new OrbitControls( camera, renderer.domElement );
-  controls.target.set(0, 0, 0);
+  controls.target.set(...startingCameraTarget);
   controls.update();
   // const loader = new GLTFLoader();
 
@@ -78,7 +158,7 @@ onMounted(() => {
     // cube.rotation.x = time / 4000
     // cube.rotation.y = time / 2000
 
-    controls.update();
+    // controls.update();
     renderer.render(scene, camera);
 
     const pos = camera.position.clone();
@@ -89,7 +169,7 @@ onMounted(() => {
       z: THREE.MathUtils.radToDeg(camera.rotation.z)
     };
 
-    displayPosition.value = {
+    cameraInfo.value = {
       position: pos,
       rotation: rot
     };
@@ -98,6 +178,13 @@ onMounted(() => {
   renderer.setAnimationLoop( animate );
   window.addEventListener('resize', handleResize);
 })
+
+function resetCamera() {
+  camera.position.set(...startingCameraPosition);
+  // camera.lookAt(...startingCameraTarget); // redundant, or worse
+  controls.target.set(...startingCameraTarget);
+  controls.update();
+}
 
 function handleResize() {
   if (!threeView.value) return
@@ -126,7 +213,8 @@ onBeforeUnmount(() => {
 
 <template>
 <div class="three-view" ref="threeView">
-  <DisplayCoordinates :position="displayPosition" />
+  <DisplayCoordinates :position="cameraInfo" />
+  <button class="absolute top-2 left-2"@click="resetCamera">Reset Camera</button>
 </div>
 </template>
 
